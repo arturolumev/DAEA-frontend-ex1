3066248cca123e61668129e8eabd5ee9f1edb504
arturolumev <edwin.lume@tecsup.edu.pe>
2023-09-20 22:40:34 -0500

    backend

--- backend/Controllers/PizzaInfoController.cs
+++ backend/Controllers/PizzaInfoController.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -38,3 +38,79 @@ namespace backend.Controllers
          }
     }
 }
+*/
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace backend.Controllers
+{
+    [Route("api/{searchTerm}")]
+    [ApiController]
+    public class GifController : ControllerBase
+    {
+        private readonly string apikey = "LIVDSRZULELA";
+        private readonly int limit = 1;
+
+        [HttpGet]
+        public async Task<IEnumerable<GifLink>> Get(string searchTerm)
+        {
+            var links = await ExtractLinks(limit, searchTerm);
+            return links;
+        }
+
+        private async Task<IEnumerable<GifLink>> ExtractLinks(int lmt, string searchTerm)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var url = $"https://g.tenor.com/v1/search?q={searchTerm}&key={apikey}&limit={lmt}";
+                var response = await httpClient.GetStringAsync(url);
+                var top_8gifs = JsonConvert.DeserializeObject<TenorResponse>(response);
+
+                var links = new List<GifLink>();
+
+                foreach (var result in top_8gifs.Results)
+                {
+                    links.Add(new GifLink
+                    {
+                        Text = result.Id,
+                        Href = result.Media[0].Webm.Preview
+                    });
+                }
+
+                return links;
+            }
+        }
+    }
+
+    public class TenorResponse
+    {
+        public List<Result> Results { get; set; }
+    }
+
+    public class Result
+    {
+        public string Id { get; set; }
+        public List<Media> Media { get; set; }
+    }
+
+    public class Media
+    {
+        public Webm Webm { get; set; }
+    }
+
+    public class Webm
+    {
+        public string Preview { get; set; }
+    }
+
+    public class GifLink
+    {
+        public string Text { get; set; }
+        public string Href { get; set; }
+    }
+}
+
